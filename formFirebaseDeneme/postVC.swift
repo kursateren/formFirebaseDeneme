351a2061645b7bdf72260d08781cf7773d746c6a
kursateren <kursaterenuzun@gmail.com>
2018-10-01 20:51:11 +0300

    first commit

---
+++ formFirebaseDeneme/postVC.swift
@@ -0,0 +1,30 @@
+//
+//  postVC.swift
+//  formFirebaseDeneme
+//
+//  Created by pasa on 27.09.2018.
+//  Copyright © 2018 kursateren. All rights reserved.
+//
+
+import UIKit
+
+class postVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
